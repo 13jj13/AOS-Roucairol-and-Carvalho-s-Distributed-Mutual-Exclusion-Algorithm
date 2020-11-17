7c0cddfb0bdc8c244c075c456986c7634a26b092
Ideeitgirl <evelyn.wong122@gmail.com>
2020-11-17 15:09:13 -0600

    Added function to combine log files

--- test/Test.java
+++ test/Test.java
@@ -1,9 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.io.*;
+import java.util.*;
 
 class Test {
     /*
@@ -36,14 +32,61 @@ class Test {
         return check;
     }
     
+    /*
+        Method: combineLogs
+        Description: Function to combine all log files from all machines into one log file
+        Parameters: String logfolder directory of where the logs are located, and String logfile output filename
+    */
+    static void combineLogs(String logfolder, String logfile) {
+        String[] fileNames;
+        File dir = new File(logfolder);
+
+        // This filter will only include files starting with log
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.startsWith("log");
+            }
+        };
+        
+        fileNames = dir.list(filter);
+        try {
+            PrintWriter pw = new PrintWriter(logfile);
+            for (String fileName : fileNames) {
+                // Print the names of files and directories
+                //System.out.println(fileName);
+                //System.out.println("Reading from " + fileName);
+
+                // create instance of file from Name of
+                // the file stored in string Array
+                File f = new File(dir, fileName);
+      
+                // create object of BufferedReader
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                
+                // Read from current file
+                String line = br.readLine();
+                while (line != null) {
+                    // write to the output file
+                    pw.println(line);
+                    line = br.readLine();
+                }
+                pw.flush();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        //System.out.println("log file populated.");
+    }
     public static void main(String[] args) throws Exception {
         try {
+            String logfile = "log.txt";
+            combineLogs(args[0], logfile);
+            // Testing methology:
             // Read in the timestamps from the log file as well as the process ID for those timestamps
-            // create a data structure  where each element of the data structure holds an enry form the log file 
-            String logfile = "";
-            logfile = args[0];
-            //System.out.println(logfile);
-            
+            // create a data structure  where each element of the data structure holds an enry form the log file
+
             File myObj = new File(logfile);
             Scanner myReader = new Scanner(myObj);
             
@@ -60,7 +103,7 @@ class Test {
                     dataMatrix.add(d);
                 }
             }
-            
+
             // sort the entries from the log file by the init timestamp in ascending order
             Collections.sort(dataMatrix, new Comparator<ArrayList<String>>() {    
                 @Override
@@ -68,6 +111,10 @@ class Test {
                     return o1.get(1).compareTo(o2.get(1));
                 }               
             });
+            
+            // print out data structure - stored accending order
+            //for (ArrayList<String> row : dataMatrix)
+            //    System.out.println(row);
 
             // Check if not implemented correctly because critical sections overlapped
             if (!checkOverlap(dataMatrix)) {
